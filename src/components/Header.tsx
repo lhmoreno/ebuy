d96c3084b796120d6cb261bb990f7c6d2da556ca
lhmoreno <lhmorenoti@hotmail.com>
2022-03-24 17:11:24 -0300

    feat: Show page active

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,29 @@
 import NextLink from 'next/link'
-import { Box, Container, Heading, Link, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { Box, Container, Heading, Link } from '@chakra-ui/react'
+
+type Pathname = '/' | '/cart' | '/login' | '/sigin' | '/forgot-password'
+type PageName = 'products' | 'cart' | 'account'
+
+const handlePageName: (pathname: Pathname) => PageName = (pathname) =>  {
+  if (pathname === '/') return 'products'
+  if (pathname === '/cart') return 'cart'
+  if (pathname === '/login') return 'account'
+  if (pathname === '/sigin') return 'account'
+  if (pathname === '/forgot-password') return 'account'
+  return 'products'
+}
 
 const Header = () => {
+  const router = useRouter()
+  const pathname = router.pathname as Pathname
+  const [pathnameActive, setPathnameActive] = useState<PageName>(handlePageName(pathname))
+
+  useEffect(() => {
+    setPathnameActive(handlePageName(pathname))
+  }, [pathname])
+
   return (
     <Container
       as="header"
@@ -22,33 +44,26 @@ const Header = () => {
         </Link>
       </NextLink>
 
-      {/* <InputGroup width="50%">
-        <InputLeftElement 
-          pointerEvents="none"
-          children={<SearchIcon color="gray.300" />}
-        />
-        <Input
-          placeholder="Buscar produto..."
-        />
-      </InputGroup> */}
-
       <Box as="nav">
         <NextLink href="/" passHref>
           <Link 
             letterSpacing="wide" 
             marginX="2"
+            fontWeight={pathnameActive === 'products' ? 'bold' : undefined}
           >Produtos</Link>
         </NextLink>
         <NextLink href="/cart" passHref>
           <Link 
             letterSpacing="wide" 
             marginX="2"
+            fontWeight={pathnameActive === 'cart' ? 'bold' : undefined}
           >Carrinho</Link>
         </NextLink>
         <NextLink href="/login">
           <Link 
             letterSpacing="wide" 
             marginX="2"
+            fontWeight={pathnameActive === 'account' ? 'bold' : undefined}
           >Conecte-se</Link>
         </NextLink>
       </Box>
