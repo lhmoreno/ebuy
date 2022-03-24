30db039353673b97278591e6170582c28468f0e1
lhmoreno <lhmorenoti@hotmail.com>
2022-03-24 17:11:50 -0300

    fix: Max price 1000

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,6 +1,11 @@
 export function formatPriceInReal(price: number) {
   let real = String(price.toFixed(2)).replace('.', ',')
-  if (price >= 1000) {
+
+  if (price >= 100000) {
+    real = real.slice(0, 3) + '.' + real.slice(3)
+  } else if (price >= 10000) {
+    real = real.slice(0, 2) + '.' + real.slice(2)
+  } else if (price >= 1000) {
     real = real.slice(0, 1) + '.' + real.slice(1)
   }
 
